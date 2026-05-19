f0476ffa60f1b6e1f7d1fa05d0fb4f1afcef419c
Dev-Shaharyar <shaharyar@instaview.ai>
2026-05-19 17:43:06 +0530

    refactor: Simplify shadow retrieval by removing database dependencies and unused code

--- shadow-fix-script/main.go
+++ shadow-fix-script/main.go
@@ -2,51 +2,29 @@ package main
 
 import (
 	"context"
-	"fix-shadow-script/db"
+	"encoding/json"
 	i "fix-shadow-script/iot"
 	"fmt"
-
-	"go.mongodb.org/mongo-driver/bson"
 )
 
-type Device struct {
-	ID      string `bson:"_id"`
-	SpaceID string `bson:"space_id"`
-}
-
 func main() {
-	atlas_connection_string := "mongodb://admin:Qq8wG62nm.W2@rs0.db.dev.instaview.ai:27017,rs1.db.dev.instaview.ai:27017/?tls=true&tlsInsecure=true"
-
-	db.NewPartnerDBs([]db.Database{
-		{Partner: "instaview", ConnectionString: atlas_connection_string},
-		{Partner: "luna", ConnectionString: atlas_connection_string}})
-
-	ctx := context.TODO()
-
-	cur, err := db.DeviceCollection("instaview").Find(ctx, bson.M{
-		"cluster_group_version": bson.M{"$ne": "v0"},
-	})
-	if err != nil {
-		println("Error finding device:", err)
-	}
-
-	var thing_name []string
-
-	for cur.Next(ctx) {
-		var device Device
-		if err := cur.Decode(&device); err != nil {
-			println("Error decoding device:", err.Error())
-			continue
-		}
-		thing_name = append(thing_name, fmt.Sprintf("%s_%s", device.SpaceID, device.ID))
-	}
+	// TEMP: get shadow for a single device
+	thingName := "36sMjQdQgZkRqQWTz5F8ASMY4Ta_cbmb3d2ui4vzzh6z32"
 
 	iotCore := i.NewIoTCore()
 
-	for _, thing := range thing_name {
-		iotCore.UpdateBitRateToEnum(thing)
+	shadow, err := iotCore.GetThingShadow(context.TODO(), thingName)
+	if err != nil {
+		fmt.Printf("Error getting shadow: %v\n", err)
+		return
 	}
 
-	fmt.Print("Updated all devices to enum bitrate\n")
+	var pretty map[string]any
+	if err := json.Unmarshal(shadow.Payload, &pretty); err != nil {
+		fmt.Printf("Raw payload: %s\n", string(shadow.Payload))
+		return
+	}
 
+	out, _ := json.MarshalIndent(pretty, "", "  ")
+	fmt.Printf("Shadow for %s:\n%s\n", thingName, string(out))
 }
